d42d057cceb99f842c1a1e6e55b6b2de7717cf9c
“rishabhrawat4” <rr987345@gmail.com>
2021-06-19 13:26:50 +0530

    material ui and Api integrated

---
+++ travclan/src/Apis/merchants.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://intense-tor-76305.herokuapp.com/merchants',
+});
---
+++ travclan/src/Components/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import merchants from '../Apis/merchants';
+import AppStyles from '../Style/AppStyle'
+
+const App = () => {
+
+  const [data, setData ] = useState(null);
+
+  const onSearchData = async() => {
+    const response = await merchants.get()
+    setData(response.data);
+  }
+
+  useEffect(() => {
+    onSearchData();
+  }, [])
+
+  const classes = AppStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Merchants List
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default App;
